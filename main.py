bce6a04cb9bb4756bd85dbc6333c2a81c6b13cfc
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2024-08-08 18:06:01 -0700

    update comments and formatting on main.py

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
+# Import modules 
 from google.cloud import storage
 from flask import Flask, request, jsonify
 
-# Initialize the Flask app
+# Initialize the Flask app 
 app = Flask(__name__)
 
-# Initialize the Google Cloud Storage client
+# Initialize the Google Cloud Storage client 
 client = storage.Client()
 
-# Name of the Google Cloud Storage bucket
+# Name of the Google Cloud Storage bucket (Change this to preexisting bucket)
 bucket_name = 'sandbox-bucket-api'
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    # Get the file from the request
-    file = request.files['file']
-    # Create a blob object for the file in the specified bucket
-    blob = client.bucket(bucket_name).blob(file.filename)
-    # Upload the file to Google Cloud Storage
-    blob.upload_from_file(file)
-    # Return a success message
-    return jsonify({'message': f'File {file.filename} uploaded successfully.'}), 200
+# End of application "boilerplate"
 
+
+
+# First API Endpoint for uploading files
+@app.route('/list', methods=['GET'])
+def list_files():
+    # List all objects (blobs/files) in the specified bucket
+    blobs = client.bucket(bucket_name).list_blobs()
+    # Create a list of file names
+    files = [blob.name for blob in blobs]
+    # Return the list of files as JSON
+    return jsonify({'files': files}), 200
+
+
+
+# Second API Endpoint for downloading files
 @app.route('/download', methods=['GET'])
 def download_file():
     # Get the filename from the request arguments
@@ -32,14 +39,27 @@ def download_file():
     # Return the file content with appropriate headers
     return content, 200, {'Content-Type': 'application/octet-stream'}
 
-@app.route('/list', methods=['GET'])
-def list_files():
-    # List all blobs (files) in the specified bucket
-    blobs = client.bucket(bucket_name).list_blobs()
-    # Create a list of file names
-    files = [blob.name for blob in blobs]
-    # Return the list of files as a JSON response
-    return jsonify({'files': files}), 200
+
+
+# First API Endpoint for uploading files
+@app.route('/upload', methods=['POST']) 
+def upload_file():
+    # Get the file from the request
+    file = request.files['file']
+    # Create a blob object for the file in the specified bucket
+    blob = client.bucket(bucket_name).blob(file.filename)
+    # Upload the file to Google Cloud Storage
+    blob.upload_from_file(file)
+    # Return a success message
+    return jsonify({'message': f'File {file.filename} uploaded successfully.'}), 200
+
+
+
+
+
+
+# Some final "boilerplate" for the python code to be wrapped by GCF function for invokations 
+
 
 # Entry point for the Cloud Function
 def main(request):
